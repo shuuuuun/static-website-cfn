e8716ce03c035ea33025c555990cb78d35bd4785
motoki-shun <motoki-shun@kayac.com>
2018-08-15 15:43:13 +0900

    リクエストログをndjsonにする

--- functions/basic_auth/index.js
+++ functions/basic_auth/index.js
@@ -13,13 +13,13 @@ exports.handler = (event, context, callback) => {
     const authorization = headers.authorization || headers.Authorization;
 
     if (ipRangeCheck(request.clientIp, allowIPs)) {
-        console.log('OK', request.clientIp, request.uri, authorization, headers.referer, userAgents, headers.pragma);
+        requestLog('OK', request);
         callback(null, request);
         return;
     }
 
     if (userAgents && allowUAs.includes(userAgents[0].value)) {
-        console.log('OK', request.clientIp, request.uri, authorization, headers.referer, userAgents, headers.pragma);
+        requestLog('OK', request);
         callback(null, request);
         return;
     }
@@ -31,25 +31,39 @@ exports.handler = (event, context, callback) => {
         for (let key in allowPasswd) {
             let val = allowPasswd[key];
             if (`${key}:${val}` === userPassword) {
-                console.log('OK', request.clientIp, request.uri, authorization, headers.referer, userAgents, headers.pragma);
+                requestLog('OK', request);
                 callback(null, request);
                 return;
             }
         }
     }
 
-    console.log('NG', request.clientIp, request.uri, authorization, headers.referer, userAgents, headers.pragma);
-
     const response = {
         status: '401',
         statusDescription: 'Authorization Required',
         httpVersion: request.httpVersion,
         headers: {
-            'www-authenticate': [{key:'WWW-Authenticate', value: 'Basic realm="Enter username and password."'}],
+            'www-authenticate': [{ key: 'WWW-Authenticate', value: 'Basic realm="Enter username and password."' }],
             'content-type': [{ key: 'Content-Type', value: 'text/plain; charset=utf-8' }],
         },
         body: '401 Authorization Required',
     };
-
+    requestLog('NG', request);
     callback(null, response);
 };
+
+function requestLog(message, request) {
+    const headers = request.headers;
+    const userAgents = headers['user-agent'];
+    const authorization = headers.authorization || headers.Authorization;
+    const format = {
+        message,
+        clientIp: request.clientIp,
+        requestUri: request.uri,
+        authorization,
+        referer: headers.referer,
+        userAgents,
+        pragma: headers.pragma,
+    };
+    console.log(JSON.stringify(format));
+}
